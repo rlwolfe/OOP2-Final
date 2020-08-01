d87e651dc91c5aa526cc86375112968f010fbde5
Rachel <67902191+rlwolfe@users.noreply.github.com>
2020-08-01 22:00:11 +0300

    Rename CollisionObjects.h to include/CollisionObjects.h

--- CollisionObjects.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<iostream>
-#include "GameObject.h"
-
-void processCollision(GameObject& object1, GameObject& object2);
---
+++ include/CollisionObjects.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<iostream>
+#include "GameObject.h"
+
+void processCollision(GameObject& object1, GameObject& object2);
